b93e4511f83d6e9a1a86eb92181ebd25f0dc893c
luscawalker <deloye98@gmail.com>
2020-11-13 10:54:13 +0100

    Première implémentation de Sender non fonctionnelle

--- app_guid_comm.c
+++ app_guid_comm.c
@@ -5,27 +5,29 @@
 #include <ivyloop.h>
 
 float roll_cmd;
-char cmd[100] = "GC_CMD_ROLL = INIT";
-bool active = True;
+char retour[100] = "GC_CMD_ROLL = INIT";
+int active = 1;
+int calcul = 0;
 
 /* fonction associe a  */
 void CalculRoulis(IvyClientPtr app, void *data, int argc, char **argv){
 	int time;
-	float XTK, TAE, DTWPT;
-	int roll_commande;
+	float XTK, TAE, DTWPT, roll_commande;
 	char retour[100] = "GC_CMD_ROLL =";
+	char tm[20], r_cmd[20];
 	const char* arg = (argc < 1) ? "" : argv[0];
 	fprintf(stderr,"%s\n",arg);
 	//GS_DATA Time="time" XTK="" TAE="" Dist_to_WPT=""
-	sscanf(arg,"GS_DATA Time=%d XTK=%d TAE=%d Dist_to_WPT=%d",time, XTK, TAE, DTWPT); // GS_XTK_TAE = time
+	sscanf(arg,"Time=%d XTK=%f TAE=%f Dist_to_WPT=%f",&time, &XTK, &TAE, &DTWPT); // GS_XTK_TAE = time
 	//fonction de check time/donnee
 	roll_commande = 200; //Calcul de la commande de roulis
+	sprintf(tm, "%d", time);
+	sprintf(r_cmd, "%f", roll_commande);
 	if(active){ //On transmet la commande si le PA est actif
-	    strcat(retour, actual_time);
-	    strcat(retour,roll_commande);
-	    IvySendMsg ("%s", retour);
-	    //roll_cmd = retour;
-	    //calcul = 0;
+	    strcat(retour, tm); //actual_time
+	    strcat(retour, r_cmd);
+	    //IvySendMsg ("%s", retour);
+	    calcul = 0;
 	}
 }
 /* fonction associe a l'horloge */
@@ -36,23 +38,28 @@ on check si on doit envoyer notre info (100ms)
 si non 
 on envoi la commande précédente
 si 1s 
-on desarme le PA
+on desarme le PA*/
 void Sender(IvyClientPtr app, void *data, int argc, char **argv){
+    int cl;
+    const char* arg = (argc < 1) ? "" : argv[0];
+    sscanf(arg,"%d",&cl);
     char retour[100] = "GC_CMD_ROLL =";
     if(calcul < 100){
-        if(clocl%100=90){ //envoi tout les 100ms à 90ms
-	        IvySendMsg ("%s", roll_cmd);
+        if((cl%100)==90){ //envoi tout les 100ms à 90ms
+	        IvySendMsg ("%s", retour);
 	        calcul++;
         }
     }
-    else{active = False;}
+    else{
+        active = 0;
+    }
 }
-
+/*
 IMPORTANT définir avec le groupe seq la periode de l'horloge 10 ou 20 ms
 */
 
 /* fonction associe a  */
-void stop (IvyClientPtr app, void *data, int argc, char **argv){
+void Stop (IvyClientPtr app, void *data, int argc, char **argv){
 	IvyStop ();
 }
 
@@ -79,12 +86,12 @@ int main (int argc, char**argv){
 	IvyInit ("Guid_COMM_APP", "Bonjour de Guid COMM", 0, 0, 0, 0);
 	IvyStart (bus);
 	/* abonnement  */
-	IvyBindMsg (CalculRoulis, 0, "GS_XTK_TAE = (.*)");
+	IvyBindMsg (CalculRoulis, 0, "GS_DATA(.*)");
 	/* abonnement */
 	IvyBindMsg (Stop, 0, "^Stop$");
 	/* abonnement */
 	//on s'abonne à l'holorge qui cadence nos envois
-	//IvyBindMsg (Clock, 0, "^
+	IvyBindMsg (Sender, 0, "^Time=");
 	/* main loop */
 	IvyMainLoop();
 	return 0;
